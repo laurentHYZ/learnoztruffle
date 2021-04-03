f512bbbf4db18eaddda4d11b0d1a67094e610d67
laurent <laurent_pinson@hotmail.com>
2021-04-03 14:47:06 +0700

    cleaned and safeguarded hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@
   module.exports = {
 +    networks: {
 +     rinkeby: {
-+       url: 'https://eth-ropsten.alchemyapi.io/v2/2GtBvvPMn1Dc1RSBrXCSniZFYUhjJqPL',
-+       accounts: {mnemonic: 'salon hat pride hub much square awesome buzz bomb giggle syrup pull'}
++       url: 'https://eth-ropsten.alchemyapi.io/v2/${alchemyApiKey}',
++       accounts: {mnemonic: mnemomics}
 +     }
 +  },
 ...
