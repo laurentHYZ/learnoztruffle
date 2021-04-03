c3c9264f928eccdbeb89551b21381a231fb38ba2
laurent <laurent_pinson@hotmail.com>
2021-04-03 14:41:14 +0700

    secrets.json added to .gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
-require('@nomiclabs/hardhat-ethers');
-
-
-module.exports = {
-  solidity: "0.8.0",
+// hardhat.config.js
++ const { alchemyApiKey, mnemonic } = require('./secrets.json');
+...
+  module.exports = {
++    networks: {
++     rinkeby: {
++       url: 'https://eth-ropsten.alchemyapi.io/v2/2GtBvvPMn1Dc1RSBrXCSniZFYUhjJqPL',
++       accounts: {mnemonic: 'salon hat pride hub much square awesome buzz bomb giggle syrup pull'}
++     }
++  },
+...
 };
